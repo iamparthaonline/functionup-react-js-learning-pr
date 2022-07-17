ae78b2f775d0a406b20477b1309ed3dab52f2f34
Partha Roy <partharoy@Parthas-MacBook-Air.local>
2022-07-17 12:25:42 +0530

    Day 2 - coding examples

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { render } from "react-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function component
+const HeadingComponent = (props) => {
+  return <h1>{props.title}</h1>;
+};
+
+class HeadingComponentUsingClass extends React.Component {
+  render() {
+    return <h1>Class Component - {this.props.title} </h1>;
+  }
+}
+
+const CurrentTIme = () => {
+  return (
+    <p>
+      {" "}
+      {+new Date()} --
+      <HeadingComponentUsingClass title=" passed from currenttime" />{" "}
+    </p>
+  );
+};
+
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <HeadingComponent title="Hello from COmponent" count="2" />
+      <CurrentTIme />
+    </React.StrictMode>
+  );
+}, 1000);
